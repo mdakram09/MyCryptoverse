25dfdd795aad10446363c7c8e14d2faa6d1e2f11
mdakram09 <iamakramkhan09@gmail.com>
2021-12-28 17:02:00 +0530

    Crypto stats and cards are created and working fine

---
+++ src/Components/Cards/HomePageCards.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import { Divider } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
+
+export default function HomePageCards() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            R
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <Divider />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          Price: $99 K
+        </Typography>
+        <br />
+        <Typography variant="body2" color="textSecondary" component="p">
+          Market Capital: $139 B
+        </Typography>
+        <br />
+        <Typography variant="body2" color="textSecondary" component="p">
+          Daily Change: -3.5%
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/Components/Homepage.jsx
@@ -0,0 +1,82 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import HomePageCards from './Cards/HomePageCards';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const Homepage = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Typography variant="h4" className="heading">
+        Global Crypto Stats
+      </Typography>
+      <br />
+
+      <div className={classes.root}>
+        <Grid
+          container
+          spacing={3}
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={6} sm={6}>
+            <Typography>Total Cryptocurrency</Typography>
+            <br />
+            <Typography variant="h6">12,999</Typography>
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            <Typography>Total Exchanges</Typography>
+            <br />
+            <Typography variant="h6">569</Typography>
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            <Typography>Total Market Cap:</Typography>
+            <br />
+            <Typography variant="h6">$2.4 T</Typography>
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            <Typography>Total 24h Volume</Typography>
+            <br />
+            <Typography variant="h6">$ 137.9 B</Typography>
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            <Typography>Total Cryptocurrencies</Typography>
+            <br />
+            <Typography variant="h6">12,999</Typography>
+          </Grid>
+          <Grid item xs={6} sm={6}>
+            <Typography>Total Markets</Typography>
+            <br />
+            <Typography variant="h6">82.2 K</Typography>
+          </Grid>
+        </Grid>
+      </div>
+
+      <div className="home-heading-container">
+        <Typography variant="h4" className="home-title">
+          Top 10 Cryptos In The World
+        </Typography>
+        <Typography className="show-more">Show more</Typography>
+      </div>
+      <br />
+
+      <HomePageCards />
+    </div>
+  );
+};
+
+export default Homepage;
