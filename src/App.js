4fb54bd4289b0b25863780d4ffc270aa03b8f839
mdakram09 <iamakramkhan09@gmail.com>
2021-12-28 21:38:44 +0530

    react router added and working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import ResponsiveDrawer from './Components/Navbar';
+import About from './Components/About';
 
 function App() {
   return (
     <>
-      <ResponsiveDrawer />
-      
+    
+    <Router>     
+        <Switch>
+        <Route exact path="/" >
+        <ResponsiveDrawer />
+          </ Route>
+          <Route exact path="/about" >
+          <About />
+          </ Route>
+        </Switch>
+    </Router>
+
+    
    </>
   );
 }
---
+++ src/Components/About.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@material-ui/core';
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <Typography>This is about page</Typography>
+    </div>
+  );
+};
+
+export default About;
