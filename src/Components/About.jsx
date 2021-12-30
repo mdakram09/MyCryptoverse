89f352881e73006f7265449f1cbbc861705d2c90
mdakram09 <iamakramkhan09@gmail.com>
2021-12-30 12:20:26 +0530

    search bar in cryptocurrency page moved to center

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,10 +1,39 @@
 import { Typography } from '@material-ui/core';
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  aboutText: {
+    textAlign: 'center',
+  },
+}));
 
 const About = () => {
+  const classes = useStyles();
+
   return (
     <div>
-      <Typography>This is about page</Typography>
+      <Typography className={classes.aboutText} variant="h4">
+        About Us
+      </Typography>
+      <br />
+      <Typography>
+        Hi, my self Md Akram Khan. I am a Final Year college student pursuing
+        BTech in Computer Science and Engineering. I have created this website
+        for practicing JavaScript, ReactJs, Material-UI, APIs, API Fetching,
+        Redux, Redux Saga, Hooks etc. You can get all the information about
+        Cryptocurrencies like their prices, exchanges, market values, news
+        related to a particular Cryptocurrency, Graphs with the data of price of
+        the Cryptocurrency as a chart, percentage change in price as a chart and
+        much more.
+      </Typography>
     </div>
   );
 };
--- src/Components/Cryptocurrency.jsx
+++ src/Components/Cryptocurrency.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import CryptocurrencyPageSearchBar from './CryptocurrencyPageSearchBar';
 import CryptocurrencyCards from './Cards/CryptocurrencyCards';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: 'absolute',
+    left: '50%',
+    top: '15%',
+    transform: 'translate(-50%, -50%)',
+  },
+}));
 
 const Cryptocurrency = () => {
+  const classes = useStyles();
+
   return (
     <>
-      <div className="search-crypto">
+      <div className={classes.root}>
         <CryptocurrencyPageSearchBar />
       </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
       <div>
         <CryptocurrencyCards />
       </div>
